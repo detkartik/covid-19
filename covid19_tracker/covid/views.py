fc808097d445a1e947fba3d94c8a16e4d2e1241e
kartik <detkartik@users.noreply.github.com>
2020-06-28 13:37:35 +0530

    Update views.py

--- covid19_tracker/covid/views.py
+++ covid19_tracker/covid/views.py
@@ -5,18 +5,22 @@ import requests
 def home(request):
     data = []
     url = "https://covid-193.p.rapidapi.com/statistics"
+    #     Getting data from api with following url
 
     querystring = {"country":"India"}
+#     Querystring contains the data specific to country (eg: India)
 
     headers = {
         'x-rapidapi-host': "covid-193.p.rapidapi.com",
         'x-rapidapi-key': "be7f37114bmsh38c0486c35a5050p1bc1e5jsnf574155ad041"
         }
-
+#      Storing in headers the api host url and keys 
     response = requests.request("GET", url, headers=headers, params=querystring).json()
-    
+#      response is storing the json data recieved from url   
     d = response['response']
+#     d converts response data into a list to get data frame
     s = d[0]
+#     s initialize the response data
 
     context = {
         'all': s['cases']['total'],
@@ -25,7 +29,11 @@ def home(request):
         'new': s['cases']['new'],
         'critical': s['cases']['critical'],
     }
+#     context gets a dict of 'all','recovered','new','critical cases'
     
    
 
     return render(request, 'index.html', context)
+'''    django templates render this context data into index.html  
+navigate to index.html to check the html display for context data
+'''
