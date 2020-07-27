ab554fc798265fd549531fac55e65c522e6770a0
Kartikey <kartikey@Kartikeys-MacBook-Pro.local>
2020-07-27 20:25:03 +0530

    naming conviention changes and some more changes

--- covid19_tracker/covid/views.py
+++ covid19_tracker/covid/views.py
@@ -1,6 +1,7 @@
-from  django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect
 from django.http import HttpResponse
 import requests
+
 
 def home(request):
     ''' 
@@ -11,28 +12,28 @@ def home(request):
     #     Getting data from api with following url
 
     # querystring is fetching country wise data and storing in dict
-    querystring = {"country":"India"}
-    
-    # header stores api keys and host path 
+    querystring = {"country": "India"}
+
+    # header stores api keys and host path
     headers = {
-        'x-rapidapi-host': "covid-193.p.rapidapi.com",
-        'x-rapidapi-key': "be7f37114bmsh38c0486c35a5050p1bc1e5jsnf574155ad041"
-        }
+        'rapidapi-host': "covid-193.p.rapidapi.com",
+        'rapidapi-key': "be7f37114bmsh38c0486c35a5050p1bc1e5jsnf574155ad041"
+    }
     # response stores the json data from the querystring
-    response = requests.request("GET", url, headers=headers, params=querystring).json()
+    response = requests.request(
+        "GET", url, headers=headers, params=querystring).json()
 
     # dataset stores the response data into dataframe/list and data_init initialize the index value of the dataframe
     dataset = response['response']
     data_init = dataset[0]
-    
+
     # context stores the data in the json formate which can be rendered by django templates
     context = {
         'all': data_init['cases']['total'],
         'recovered': data_init['cases']['recovered'],
         'deaths': data_init['deaths']['total'],
         'new': data_init['cases']['new'],
-        'critical': data_init['cases']['critical'],
     }
     # context gets a dict of 'all','recovered','new','critical cases'
-    
+
     return render(request, 'index.html', context)
